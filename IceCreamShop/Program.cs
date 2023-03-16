1da6b3d0c97ab0da5f0ed0e4dc2fead722b274e2
Alaina Balducci <alainabalducci@gmail.com>
2023-03-16 16:10:18 -0500

    Completed ice cream shop exercise

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -6,8 +6,41 @@ List<Cone> availableCones = menu.Cones;
 
 // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
 //  field.
+//FlavorComparer comparer = new FlavorComparer();
+
+
+//foreach(Flavor flavor in availableFlavors)
+//{
+//    Console.WriteLine(flavor.Name);
+//}
+
+//Console.WriteLine("*************************");
+
+availableFlavors.Sort(new FlavorComparer());
+
+foreach (Flavor flavor in availableFlavors)
+{
+    Console.WriteLine($"Flavor Name: {flavor.Name} - Allergens: {flavor.Allergens.Count}");
+}
+Console.WriteLine("*************************");
+
 
 // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
 //  field.
+//foreach (Cone cone in availableCones)
+//{
+//    Console.WriteLine($"{cone.Name}: {cone.Cost}");
+//}
+
+//Console.WriteLine("*************************");
+
+availableCones.Sort(new ConeComparer());
+
+foreach (Cone cone in availableCones)
+{
+    Console.WriteLine($"{cone.Name}: {cone.Cost}");
+    
+}
 
 // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
